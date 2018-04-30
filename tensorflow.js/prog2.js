aaf05ef3a46c0203572781f16055d22fc830ac20
envzhu <densikousakusyosinsya@gmail.com>
2018-04-30 17:58:01 +0900

    change lerning rate in TensorFlow.js/prog2.js

--- tensorflow.js/prog2.js
+++ tensorflow.js/prog2.js
@@ -25,7 +25,7 @@ window.addEventListener("DOMContentLoaded", function() {
     //y = A * x^2 + B * x + C
     let y = x => tf.add(tf.add(tf.mul(A,x.square()),tf.mul(B,x)),C);
     let loss = (y,yd) => y.sub(yd).square().mean();
-    let optimizer = tf.train.sgd(0.01);
+    let optimizer = tf.train.sgd(0.5);
     console.log(`step: 0, A: ${A.dataSync()}, B: ${B.dataSync()}, C: ${C.dataSync()}`);
     for (let i = 1; i <= 5000; i++) {
       optimizer.minimize(() => loss(y(xData),yData))
